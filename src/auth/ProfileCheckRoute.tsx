d27124cd27244218144d1b22b2b6e8a99c093460
AshishNith <ranjan.ashish9992@gmail.com>
2025-09-07 17:12:52 +0530

    update

--- src/auth/ProfileCheckRoute.tsx
+++ src/auth/ProfileCheckRoute.tsx
@@ -50,7 +50,7 @@ const ProfileCheckRoute: React.FC<ProfileCheckRouteProps> = ({
 
   // Handle loading state
   if (isLoading || isCheckingProfile) {
-    return <div className="flex items-center justify-center h-screen">Loading...</div>;
+    return <div className="flex items-center bg-[#030714] justify-center h-screen">Loading...</div>;
   }
 
   // If user is not logged in, redirect to login
